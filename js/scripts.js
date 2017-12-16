a1fbc9903d0d78468ae91cc0131e5d5c61881da9
Sam Gespass <30681859+darthtoad@users.noreply.github.com>
2017-12-15 16:46:40 -0800

    refactor with array of pizza objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-var toppingsArray = [];
+var pizzas = [];
+var final = 0;
 
 var toppingsObj = {
   pepperoni: 2,
@@ -20,29 +21,36 @@ var sizeObj = {
 
 var number = 0;
 
-var Pizza = function(size, toppings, number) {
+var Pizza = function(size, toppings) {
   this.size = size;
   this.toppings = toppings;
-  //this.number = number;
   this.price = 0;
 }
 
 Pizza.prototype.calculatePrice = function() {
   var sizesTotal = 0;
   for (var key in sizeObj) {
-    this.size.forEach(function(value) {
-      if (key === value) {
+    pizzas.forEach(function(value) {
+      if (key === value.size) {
         sizesTotal += sizeObj[key];
       }
     })
   }
   var toppingsTotal = 0;
   for (var key in toppingsObj) {
-    this.toppings.forEach(function(value) {
-      if (key === value) {
-        toppingsTotal += toppingsObj[key];
-      }
+    pizzas.forEach(function(value){
+      console.log(value.toppings);
+      value.toppings.forEach(function(topping) {
+        if (key === topping) {
+          toppingsTotal += toppingsObj[key];
+        }
+      })
     })
+    // this.toppings.forEach(function(value) {
+    //   if (key === value.toppings) {
+    //     toppingsTotal += toppingsObj[key];
+    //   }
+    // })
   }
   this.price += toppingsTotal + sizesTotal;
   //this.price *= this.number;
@@ -51,6 +59,7 @@ Pizza.prototype.calculatePrice = function() {
 $(document).ready(function(){
   $("form#number-input").submit(function(event){
     event.preventDefault();
+    final = 0;
     $("#result").empty();
     number = $("input#number").val();
     if (!number) {
@@ -102,20 +111,37 @@ $(document).ready(function(){
   $("form#order").submit(function(event){
     event.preventDefault();
     $("#result").empty();
-    toppingsArray = [];
-    var sizes = [];
+    var allToppings = [];
+    var allSizes = [];
+    pizzas = [];
     for (i = 0; i < number; i++) {
       var size = $("input:radio[name=size" + i + "]:checked").val();
-      sizes.push(size);
+      allSizes.push(size);
     }
+      //Object.defineProperty(allSizes, i, $("input:radio[name=size" + i + "]:checked").val());
+      /*var size = $("input:radio[name=size" + i + "]:checked").val();
+      sizes.push(size);*/
     for (i = 0; i < number; i++) {
+      var currentToppings = [];
+      $("input:checkbox[name=toppings" + i + "]:checked").each(function(){
+        currentToppings.push($(this).val());
+      })
+      allToppings.push(currentToppings);
+      /*Object.defineProperty(allToppings, i, $("input:checkbox[name=toppings" + i + "]:checked").each(function(){
+        $(this).val();
+      }))
+      /*
       $("input:checkbox[name=toppings" + i + "]:checked").each(function(){
         toppingsArray.push($(this).val());
-      })
+      })*/
     }
-
-    var newPizza = new Pizza(sizes, toppingsArray/*, number*/);
-    newPizza.calculatePrice();
+    console.log(allToppings);
+    for (i = 0; i < number; i++) {
+      var newPizza = new Pizza(allSizes[i], allToppings[i]);
+      pizzas.push(newPizza);
+      final += newPizza.calculatePrice();
+    }
+    //var newPizza = new Pizza(sizes, toppingsArray);
     $("#result").append("<br><br>Your total is $" + newPizza.price + "<br><br>");
   })
 })
