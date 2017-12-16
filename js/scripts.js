e3733702bed9c68a8535cb4fec508e0b544c2089
Sam Gespass <30681859+darthtoad@users.noreply.github.com>
2017-12-16 11:24:42 -0800

    remove console logs and debugger

--- js/scripts.js
+++ js/scripts.js
@@ -30,16 +30,12 @@ var Pizza = function(size, toppings) {
 }
 
 Pizza.prototype.calculatePrice = function() {
-  //debugger;
   for (var key in sizeObj) {
     var currentSize = 0;
     for (value in pizzas[pizzaNumber]) {
-      console.log(key);
-      console.log(value);
       if (value === "size") {
         if (key === pizzas[pizzaNumber].size) {
           currentSize = sizeObj[key];
-          console.log("eiorfjfoewj")
         }
       }
     }
@@ -51,8 +47,6 @@ Pizza.prototype.calculatePrice = function() {
     for (value in pizzas[pizzaNumber]) {
       if (value === "toppings") {
         pizzas[pizzaNumber].toppings.forEach(function(topping){
-          console.log(key);
-          console.log(topping);
           if (key === topping) {
             currentToppings = toppingsObj[key];
           }
@@ -62,35 +56,7 @@ Pizza.prototype.calculatePrice = function() {
     this.price += currentToppings;
     final += currentToppings;
   }
-    /*pizzas.forEach(function(value) {
-      if (key === value.size) {
-        currentSize = sizeObj[key];
-      }
-    })
-    this.price += currentSize;
-    final += currentSize;
-  }
-  for (var key in toppingsObj) {
-    var currentToppings = 0;
-    pizzas.forEach(function(value){
-      value.toppings.forEach(function(topping) {
-        if (key === topping) {
-          currentToppings = toppingsObj[key];
-        }
-      })
-    })
-    this.price += currentToppings;
-    final += currentToppings;*/
-    // this.toppings.forEach(function(value) {
-    //   if (key === value.toppings) {
-    //     toppingsTotal += toppingsObj[key];
-    //   }
-    // })
 }
-  //this.price = currentToppings + currentSize;
-  //final += currentToppings + currentSize;
-
-  //this.price *= this.number;
 
 $(document).ready(function(){
   $("form#number-input").submit(function(event){
@@ -154,33 +120,19 @@ $(document).ready(function(){
       var size = $("input:radio[name=size" + i + "]:checked").val();
       allSizes.push(size);
     }
-      //Object.defineProperty(allSizes, i, $("input:radio[name=size" + i + "]:checked").val());
-      /*var size = $("input:radio[name=size" + i + "]:checked").val();
-      sizes.push(size);*/
     for (i = 0; i < number; i++) {
       var currentToppings = [];
       $("input:checkbox[name=toppings" + i + "]:checked").each(function(){
         currentToppings.push($(this).val());
       })
       allToppings.push(currentToppings);
-      /*Object.defineProperty(allToppings, i, $("input:checkbox[name=toppings" + i + "]:checked").each(function(){
-        $(this).val();
-      }))
-      /*
-      $("input:checkbox[name=toppings" + i + "]:checked").each(function(){
-        toppingsArray.push($(this).val());
-      })*/
     }
-    console.log(allToppings);
-    console.log(allSizes);
     for (i = 0; i < number; i++) {
       var newPizza = new Pizza(allSizes[i], allToppings[i]);
-      console.log(newPizza);
       pizzas.push(newPizza);
       newPizza.calculatePrice();
       pizzaNumber = pizzaNumber + 1;
     }
-    //var newPizza = new Pizza(sizes, toppingsArray);
     $("#result").append("<br><br>Your total is $" + final + "<br>" + "You ordered:<br>");
     pizzas.forEach(function(pizza){
       $("#result").append("A " + pizza.size + " pizza with " + pizza.toppings.join(" and ") + ". The price for this pizza is $" + pizza.price + "<br>");
@@ -193,7 +145,7 @@ $(document).ready(function(){
     var ifDelivery = $("input:radio[name=obtain]:checked").val();
     if (ifDelivery === "pick-up") {
       $("#address").hide();
-      $("#result").append("<br>Your order will be ready in 30 minutes")
+      $("#result").append("<br>Your order will be ready in 30 minutes.")
     } else {
       $("#address").hide();
       $(".address").show();
@@ -218,8 +170,6 @@ $(document).ready(function(){
     }
   })
   $("form.address").submit(function(event){
-    debugger;
-    console.log("success");
     event.preventDefault();
     $(".address").hide();
     $("#result").append("Your order will be sent to: <br>" + $("input.street").val() + "<br>" + $("input.city").val() + ", " + $("input.state").val() + " " + $("input.zip-code").val() + "<br>It should arrive within 45 minutes.");
